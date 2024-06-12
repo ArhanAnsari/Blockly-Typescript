acf836eb28b78d139936f8c0e34db45a4703179c
Arhan Ansari <arhanansari2009@gmail.com>
2024-06-12 14:58:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blockly
+
+Blockly is a library that adds a visual code editor to web and mobile apps. The Blockly editor uses interlocking, graphical blocks to represent code concepts like variables, logical expressions, loops, and more. It allows users to apply programming principles without having to worry about syntax or the intimidation of a blinking cursor on the command line.
+
+Guide: https://developers.google.com/blockly/guides/overview
---
+++ main.ts
@@ -0,0 +1,37 @@
+import Blockly from 'blockly';
+
+const toolbox = {
+  "kind": "flyoutToolbox",
+  "contents": [
+    {
+      "kind": "block",
+      "type": "controls_if"
+    },
+    {
+      "kind": "block",
+      "type": "controls_repeat_ext"
+    },
+    {
+      "kind": "block",
+      "type": "logic_compare"
+    },
+    {
+      "kind": "block",
+      "type": "math_number"
+    },
+    {
+      "kind": "block",
+      "type": "math_arithmetic"
+    },
+    {
+      "kind": "block",
+      "type": "text"
+    },
+    {
+      "kind": "block",
+      "type": "text_print"
+    },
+  ]
+};
+
+const workspace = Blockly.inject('blockly', { toolbox: toolbox });
